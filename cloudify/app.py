b496305eb396c11da311980b2ec494a6f045dd4a
Stefan Berggren <nsg@nsg.cc>
2015-05-28 17:35:56 +0200

    Example mocked version of the output.

--- cloudify/app.py
+++ cloudify/app.py
@@ -3,6 +3,8 @@ from datetime import datetime
 import urllib2
 import json
 import re
+import random
+import uuid
 
 app = Flask(__name__)
 
@@ -10,9 +12,18 @@ app = Flask(__name__)
 def index():
     return 'Index'
 
-@app.route('/data') #methods=['POST'])
-def names():
-    return jsonify([])
+@app.route('/tags')
+def tags():
+    tags = []
+    for i in range(10):
+        tag = {
+            "name" : str(uuid.uuid4())[0:5],
+            "num"  : random.randint(1, 100)
+        }
+        tags.append(tag)
+    data = { "tags": tags }
+
+    return jsonify(data)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0',debug=True)
