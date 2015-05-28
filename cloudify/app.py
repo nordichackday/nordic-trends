b89dc9ceeb3e92136030d73b2e7c8bd6de998938
Stefan Berggren <nsg@nsg.cc>
2015-05-28 17:41:17 +0200

    Move to separate function.

--- cloudify/app.py
+++ cloudify/app.py
@@ -14,6 +14,12 @@ def index():
 
 @app.route('/tags')
 def tags():
+    tags = random_output()
+    data = { "tags": tags }
+
+    return jsonify(data)
+
+def random_output():
     tags = []
     for i in range(10):
         tag = {
@@ -21,9 +27,7 @@ def tags():
             "num"  : random.randint(1, 100)
         }
         tags.append(tag)
-    data = { "tags": tags }
-
-    return jsonify(data)
+    return tags
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0',debug=True)
