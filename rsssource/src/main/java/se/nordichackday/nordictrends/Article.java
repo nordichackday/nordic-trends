f087a8fb81249aeaa31a75bb6242568ecbdd3df8
Anna Gomez <anna.gomez@svt.se>
2015-05-29 10:06:39 +0200

    some changes, and delete files that shouldn\'t have been committed

--- rsssource/src/main/java/se/nordichackday/nordictrends/Article.java
+++ rsssource/src/main/java/se/nordichackday/nordictrends/Article.java
@@ -2,12 +2,24 @@ package se.nordichackday.nordictrends;
 
 public class Article {
     private final String title;
+    private final String description;
+    private final String url;
 
-    public Article(String title) {
+    public Article(String title, String description, String url) {
         this.title = title;
+        this.description = description;
+        this.url = url;
     }
 
     public String getTitle() {
         return title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public String getUrl() {
+        return url;
     }
 }
