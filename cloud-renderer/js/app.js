777a1f0f20a1e857b5747d6093eecf5e001ac18b
Alexander Bethke <alexander.bethke@svt.se>
2015-05-29 14:43:11 +0200

    Uses proper backend

--- cloud-renderer/js/app.js
+++ cloud-renderer/js/app.js
@@ -1,7 +1,8 @@
 var App = angular.module('App', []);
 
 App.controller('nordicTrends', function($scope, $http) {
-  $http.get('http://localhost:3000/tags')
+  $http.get('http://localhost:3000/tags/3')
+  // $http.get('http://localhost:3000/random')
        .then(function(res){
           var flatList = res.data.tags;
           $scope.nordicTrendItems = _.groupBy(flatList, function(tag){ return tag.origin; });
